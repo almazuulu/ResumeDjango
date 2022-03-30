06bd5371cfd7999c81d8981e4e7ad118cb099389
almazuulu <almazuulu@gmail.com>
2022-03-30 16:38:33 +0600

    Created sample templates

--- resumesite/projects/views.py
+++ resumesite/projects/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def projects(request):
-    return HttpResponse("Hello from projects")
+    #return HttpResponse("Hello from projects")
+    return render(request, 'projects.html')
 
 def project(request, pk):
-    return HttpResponse("Hello there!" + " " + str(pk))
+    #return HttpResponse("Hello there!" + " " + str(pk))
+    return render(request, 'single-project.html')
 
 
