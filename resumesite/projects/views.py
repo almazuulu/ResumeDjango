fdf2d96ec9c1d3d9a0aae3da3e6ad281d4be907c
almazuulu <almazuulu@gmail.com>
2022-03-30 23:29:53 +0600

    Working with template and view

--- resumesite/projects/views.py
+++ resumesite/projects/views.py
@@ -2,12 +2,47 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional Ecommerce Platform'
+    },
+
+    {
+        'id': '2',
+        'title': 'Project Protfolio',
+        'description': 'My Prject protfolio'
+    },
+
+    {
+        'id': '3',
+        'title': 'Some another portfolio',
+        'description': 'Last project description'
+    },
+
+
+
+]
+
 def projects(request):
-    #return HttpResponse("Hello from projects")
-    return render(request, 'projects.html')
+    someName  = 'Askarbek'
+    context = {
+        'name': someName,
+        'projectslist': projectsList
+    }
+    return render(request, 'projects/projects.html', context= context)
 
 def project(request, pk):
-    #return HttpResponse("Hello there!" + " " + str(pk))
-    return render(request, 'single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+
+    context = {
+        'project': projectObj
+    }
+
+    return render(request, 'projects/single-project.html', context= context)
 
 
