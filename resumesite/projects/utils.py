e80ab1de17c251a56ab422ba30cfba851492e2fa
almazuulu <almazuulu@gmail.com>
2022-04-18 02:25:35 +0600

    Поиск проекта по разработчику, по тэгу

--- resumesite/projects/utils.py
+++ resumesite/projects/utils.py
@@ -3,11 +3,15 @@ from django.db.models import Q
 
 def search_project(request):
     search_query = ''
-
+    tag = Tag.objects.filter(name__icontains = search_query)
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
 
-    all_projects = Project.objects.filter(Q(title__icontains=search_query) |
-                                          Q(description__icontains=search_query))
+    all_projects = Project.objects.distinct().filter(
+        Q(title__icontains=search_query) |
+        Q(description__icontains=search_query) |
+        Q(owner__name__icontains=search_query) |
+        Q(tags__in = tag)
+    )
 
     return all_projects, search_query
